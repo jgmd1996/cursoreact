ff2ebe4d1758177dc0e0c515b9eb7304bb1d94d0
Your Name <you@example.com>
2021-07-06 03:01:04 -0300

    feito a parte de home

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import React, { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import { BrowserRouter as Router, Route, route } from 'react-router-dom';
 
+import Header from './components/Header';
 import Tasks from "./components/Tasks";
 import AddTask from "./components/AddTask";
 import "./App.css";
@@ -18,16 +21,26 @@ const App = () => {
       title: "ler livros",
       completed: true,
     },
-    
+
   ]);
-  
+
+  const handleTaskClick = (taskId) => {
+    const newTasks = tasks.map((task) => {
+      if (task.id === taskId) return { ...task, completed: !task.completed };
+
+      return task;
+    });
+
+    setTasks(newTasks)
+  };
+
   const handleTaskAddition = (taskTitle) => {
     console.log(taskTitle)
     const newTasks = [
       ...tasks,
       {
         title: taskTitle,
-        id: Math.random(10),
+        id: uuidv4(),
         completed: false,
       },
     ];
@@ -35,14 +48,32 @@ const App = () => {
     setTasks(newTasks);
   };
 
+  const handleTaskdeletion = (taskId) => {
+    const newTasks = tasks.filter(task => task.id !== taskId)
+
+    setTasks(newTasks)
+  }
+
   return (
-    <>
-    <div className = "container">
-      <AddTask handleTaskAddition={handleTaskAddition}/>
-      <Tasks tasks={tasks} />
-    </div>
-      
-    </>
+    <Router>
+      <div className="container">
+        <Header />
+        <Route
+          path="/"
+          exact
+          render={() => (
+            <>
+              <AddTask handleTaskAddition={handleTaskAddition} />
+              <Tasks
+                tasks={tasks}
+                handleTaskClick={handleTaskClick}
+                handleTaskdeletion={handleTaskdeletion}
+              />
+            </>
+          )}
+        />
+      </div>
+    </Router>
   );
-} 
+};
 export default App;
--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,10 +1,31 @@
 import React from 'react';
+import { CgClose, CgInfo } from "react-icons/cg";
 
 import './Task.css'
-const Task = ({task}) => {
-    return <div className="task-container">{task.title}
+const Task = ({ task, handleTaskClick, handleTaskdeletion }) => {
+    return (
+        <div className="task-container"
+            style={task.completed ? { borderLeft: '6px solid chartreuse' } : {}}
+        >
+            <div className="task-title" onClick={() => handleTaskClick(task.id)}>
+                {task.title}
+            </div>
 
-    </div>
-}
+            <div className="buttons-container">
+                <button
+                    className="remove-task-button"
+                    onClick={() => handleTaskdeletion(task.id)}
+                >
+                    <CgClose />
+                </button>
+                <button
+                    className="see-task-details-button"
+                >
+                    <CgInfo />
+                </button>
+            </div>
+        </div>
+    );
+};
 
-export default  Task;
+export default Task;
